08097cd85676c0f557de1d3dcff4d7ed297b7c13
TanpureSaiprasad <138004794+TanpureSaiprasad@users.noreply.github.com>
2023-07-10 16:00:28 +0530

    Update SET-A_Q3.php

--- php Assignment 1(besic in php)/SET-A_Q3.php
+++ php Assignment 1(besic in php)/SET-A_Q3.php
@@ -1,5 +1,5 @@
 <?php
-//Write a PHP Script which will convert temperatures from Celsius(C)to Fahrenheit (F). (Hint: C=5.0/9(F-32)
+//Q.Write a PHP Script which will convert temperatures from Celsius(C)to Fahrenheit (F). (Hint: C=5.0/9(F-32)
     $f= 30.90;
     $c=5.0/9*($f-32);
          echo("temperature in farhanite: $f ");
